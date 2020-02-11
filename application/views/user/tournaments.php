a187733e5244b536f912a4ed10b26cf8035a294e
Varun Mee <meevarun@Varuns-MacBook-Pro.local>
2020-02-11 16:19:58 +0530

    minor changes in tournament page

--- application/views/user/tournaments.php
+++ application/views/user/tournaments.php
@@ -133,7 +133,7 @@
             <div class="animated fadeIn">
 
                 <div class="row">
-                    <div class="col-lg-6">
+                    <div class="col-lg-12">
                         <div class="card">
                             <div class="card-header bg-white">
                                 <h4>Tournaments</h4>
